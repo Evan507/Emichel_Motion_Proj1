8d5989a7000978f9aa7f2b308933c9e4795f08fb
Evan <81766815+Evan507@users.noreply.github.com>
2022-04-19 12:12:51 -0400

    timing on effect-fine tuning

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,5 +4,7 @@ const heromove = gsap.timeline();
     
 heromove
 .from(".abyss-hero", {duration: 3, y: 450}, "abyss-rise")
-.from(".abyss-hero", {duration: 4, opacity: 0}, "abyss-rise")
-.from(".history-hero", {delay: 2, duration: 2, opacity: 0}, "abyss-rise")
+.from(".abyss-hero", {duration: 6, opacity: 0}, "abyss-rise")
+.from(".history-hero", {delay: 2.2, duration: 2, opacity: 0}, "abyss-rise")
+
+;
